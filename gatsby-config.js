d9a7cddea9ee0578da67b23dd0a1546c030aa891
EricKuang <erickuang@EricKuangdeMacBook-Pro.local>
2021-01-27 11:12:03 +0800

    ignore bad building with module

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: "kuang.admin",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "MJqBkJsTcQshL5qnYAulmGgvuqrR5TuAskvsJx_hVHU",
-        spaceId: "",
-      },
-    },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		title: "kuang.admin",
+	},
+	plugins: [{
+			resolve: "gatsby-source-contentful",
+			options: {
+				accessToken: "43K5RKnyv6J95QNpbi933maDh4S1oyZcl9uaNndIfNE",
+				spaceId: "9bg4qqqqg23b",
+			},
+		},
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-sharp",
+		"gatsby-plugin-react-helmet",
+		"gatsby-transformer-sharp",
+		{
+			resolve: `gatsby-plugin-styled-components`,
+			options: {
+				// Add any options here
+			},
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: `gatsby-plugin-typescript`,
+			options: {
+				isTSX: true, // defaults to false
+				jsxPragma: `jsx`, // defaults to "React"
+				allExtensions: true, // defaults to false
+			},
+		},
+	],
 };
